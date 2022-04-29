f1f21e6beea6c941699e142146bb75850e2b0dc1
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2022-04-29 09:33:25 -0500

    Fixing associated dependency bug

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
-                'dependency' => $request->user()->getAssociatedDependency()
+                'dependency' => $request->user() ?? $request->user()->getAssociatedDependency()
             ],
         ]);
     }
